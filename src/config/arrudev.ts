88441def04aa423b5b931b051eec63bb5d9bf509
João Arruda <joaoarruda.info@gmail.com>
2023-04-17 16:41:58 -0300

    main: adicionado menu responsivo e adicionado configuração

---
+++ src/config/arrudev.ts
@@ -0,0 +1,30 @@
+import {
+    ArrowPathIcon,
+    Bars3Icon,
+    ChartPieIcon,
+    CursorArrowRaysIcon,
+    FingerPrintIcon,
+    SquaresPlusIcon,
+    XMarkIcon,
+} from '@heroicons/react/24/outline'
+
+export const Arrudev = {
+    name: 'Arrudev',
+    imageLogo: '',
+    menu:
+        [
+            {
+                icon: '',
+                title: '',
+                submenu: [
+                    {
+                        name: 'Joao',
+                        description: 'Get a better understanding of your traffic',
+                        href: '#',
+                        icon: ChartPieIcon,
+                    }
+                ]
+            },
+        ]
+
+}
